f7b64e033cbeaba7935757cac546da5bdc4ad728
Ainy Afzal <ainyafzaldev@gmail.com>
2023-09-25 13:02:57 -0400

    reorganized structure

---
+++ README.md
@@ -0,0 +1,8 @@
+### `npm start`
+
+Builds the frontend and runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+All routes are in index.js under routes folder
+All views are created in a React app under viewsd folder, public folder exists under view folder.
+
--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ const path = require('path')
 const app = express()
 const port = 3000
 
-app.use(express.static(path.join(__dirname, "frontend", "build")))
+app.use(express.static(path.join(__dirname, "..", "views", "build")))
 
 app.get("/api", async (req, res) => {
   res.send({ message: "Hello"})
 })
 app.get("*", async(req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
+  res.sendFile(path.join(__dirname, "..", "views", "build", "index.html"))
 })
 
 app.listen(port, () => {
